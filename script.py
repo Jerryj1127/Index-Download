0529e44d1a73ca89c24d1b9e0ab766a75760d021
Jerry <Jerryj1127@gmail.com>
2020-09-12 23:04:51 +0530

    A few minor fixes

--- script.py
+++ script.py
@@ -1,34 +1,44 @@
 import requests
+import urllib
 from bs4 import BeautifulSoup
+import time
+start_time = time.time()
+
 
 server = 'http://103.126.12.226/Data/disk4/Hollywood/16-11-19/'
 
-stack = []
-queue = []
+links = []
+directories = []
+
+directories.append(server)
 
 def download(site_url):
 
-    data = requests.get(site_url)
-    soup = BeautifulSoup(data.text)
+    html = urllib.request.urlopen(site_url).read()
+    soup = BeautifulSoup(html,"html.parser")
+    #           OR
+    #data = requests.get(site_url)
+    #soup = BeautifulSoup(data.text, "html.parser")
     
     tags = soup('a')
     for tag in tags[1:]:
         link = tag.get('href', None)
         if not link.startswith(site_url):
             link = site_url+link
-        if link.upper().endswith((".MKV", ".MP4", ".WEBM", ".AVI", ".MOV", ".AVCHD", ".FLV", ".WMV")):
-            stack.append(link)
+        if link.upper().endswith((".MKV", ".MP4", ".WEBM", ".AVI", ".MOV", ".AVCHD", ".FLV", ".WMV", ".SRT")):
+            links.append(link)
             print(link)
         elif link.endswith('/'):
-            queue.append(link)
+            directories.append(link)
 
-download(server)
-for item in queue:
+for item in directories:
     download(item)
 
-print('Downloadable links:', len(stack))
-print('Root directories found', len(queue))
+print('Downloadable links:', len(links))
+print('Root directories found', len(directories)-1)
 
 with open('ftp_links.txt', 'w') as f:
-    for item in stack:
+    for item in links:
         f.write(item + '\n')
+
+print("\n--- %s seconds ---" % (time.time() - start_time))
